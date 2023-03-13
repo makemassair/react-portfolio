d05c6adbace319a256432bd9e320f8fb469cafb7
Michael Samaras <mike@makemassair.co.uk>
2023-03-13 19:46:11 +0000

    Working (basic) contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,44 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 
-const ContactForm = () => {
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
+function ContactForm() {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "a7338364-67fd-4377-948c-d1f2b93baf31");
+
+    const res = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
+      body: formData
+    }).then((res) => res.json());
+
+    if (res.success) {
+      console.log("Success", res);
+      setResult(res.message);
+    } else {
+      console.log("Error", res);
+      setResult(res.message);
+    }
   };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
+    <div className="App">
+      <h1>React File Upload Form</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="name"/>
+        <input type="email" name="email"/>
+        <textarea name="message"></textarea>
+        <input type="submit" />
+      </form>
+      <span>{result}</span>
+    </div>
   );
-};
+}
 
 export default ContactForm;
