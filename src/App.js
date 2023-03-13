0aa067698054176519ab567a502445f58c94e721
Michael Samaras <mike@makemassair.co.uk>
2023-03-13 17:19:42 +0000

    Added NotFound / 404 component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-    <Header />
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     
     <Footer />
     </div>
+    </Router>
     );
 }
 
