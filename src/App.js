6f239142b27f0850ec8694caf624b1e43810d05e
Michael Samaras <mike@makemassair.co.uk>
2023-03-13 18:28:28 +0000

    added form templating to Contact.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import Footer from "./Footer";
 import Header from "./Header";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useEffect } from "react";
+import Home from './Home';
+import Projects from './Projects';
+import Contact from './Contact';
+import NotFound from './NotFound';
 
 function App() {
+  useEffect(() => {
+    if (document) {
+      const stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.href = "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+
+      document.head.appendChild(stylesheet);
+    }
+  }, []);
+
   return (
     <Router>
     <div className="App">
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Michael Frank Samaras</h1>
             <div className="links"> 
+            <Link to="/">Home</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
             </div>
         </nav>
      );
