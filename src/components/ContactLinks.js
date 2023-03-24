249016212fc5a875f30f812aeb91911081253624
Michael Samaras <mike@makemassair.co.uk>
2023-03-24 15:29:09 +0000

    added Instagram link

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -3,6 +3,7 @@ const ContactLinks = () => {
         <div className="contact-links">
             <a href="mailto:mike@makemassair.co.uk" className="contact-email" target="_blank" rel="noopener noreferrer" aria-label="link to email">Email</a>
             <a href="https://github.com/makemassair" className="contact-github"target="_blank" rel="noopener noreferrer" aria-label="link to GitHub">GitHub</a>
+            <a href="https://instagram.com/makemassair" className="contact-insta" target="_blank" rel="noopener noreferrer" aria-label="link to instagram">Instagram</a>
             <a href="https://www.linkedin.com/in/mikesamaras/" className="contact-linkedin" target="_blank" rel="noopener noreferrer" aria-label="link to LinkedIn">LinkedIn</a>
         </div>
      );
